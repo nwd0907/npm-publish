9260435ed1093e7c840b918797f56ee55de074de
nwd0907 <nwd0907@newbizstart.com>
2022-07-07 23:57:20 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports.getToday = () => {
+    const date = new Date()
+    const yyyy = date.getFullYear()
+    const mm = date.getMonth() + 1
+    const dd = date.getDate()
+    return `${yyyy}-${mm}-${dd}`
+}
